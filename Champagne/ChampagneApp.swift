8122a9517582e99c8a22d95d36f1854b82f478a7
Logan Voss <loganvoss714@gmail.com>
2026-02-06 08:49:58 -0800

    Initial Commit

---
+++ Champagne/ChampagneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChampagneApp.swift
+//  Champagne
+//
+//  Created by Logan Voss on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ChampagneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Champagne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Champagne
+//
+//  Created by Logan Voss on 2/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
